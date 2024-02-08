263b8cf9ab04dad76406671839bd9b2fdfb1b73f
Ikeji Benjamin <benjaminikeji@gmail.com>
2024-02-08 05:10:50 +0100

    Add address input

--- src/pages/GenerateTransaction.tsx
+++ src/pages/GenerateTransaction.tsx
@@ -11,14 +11,18 @@ import {
   NumberInputField,
   NumberInputStepper,
   NumberIncrementStepper,
+  InputGroup,
+  Input,
+  InputRightElement,
 } from '@chakra-ui/react';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import * as yup from 'yup';
 import CopySvg from '@/assets/Copy.svg';
 import Button from '@/components/Button';
 import Card from '@/components/Card';
+import GenerateAddress from '@/components/GenerateAddress';
 import PageTitle from '@/components/PageTitle';
 import PlusCircle from '@/components/PlusCircle';
 import { Select, KeyTypeOption, AssetOption } from '@/components/select';
@@ -34,8 +38,22 @@ const helperTextProps: ChakraProps = {
 };
 
 const schema = yup.object().shape({
-  keyType: yup.string().required('This is required'),
-  asset: yup.string().required('This is required'),
+  keyType: yup
+    .object()
+    .shape({
+      value: yup.string().required('Key type is required'),
+      label: yup.string().required('Key type is required'),
+    })
+    .required('This is required'),
+  asset: yup
+    .object()
+    .shape({
+      value: yup.string().required('Please select an asset'),
+      label: yup.string().required('Please select an asset'),
+    })
+    .required('This is required'),
+  amount: yup.number().required('This is required'),
+  address: yup.string().required('This is required'),
 });
 
 const GenerateTransaction = () => {
@@ -43,15 +61,21 @@ const GenerateTransaction = () => {
     register,
     handleSubmit,
     formState: { errors = {} },
+    control,
   } = useForm({
     mode: 'all',
     resolver: yupResolver(schema),
   });
 
   const [isAmountInputFocused, setIsAmountInputFocused] = useState(false);
+  const [isAddressInputFocused, setIsAddressInputFocused] = useState(false);
 
   const handleAmountInputFocus = () => {
     setIsAmountInputFocused(true);
+  };
+
+  const handleAddressInputFocus = () => {
+    setIsAddressInputFocused(true);
   };
 
   const onSubmitForm = (values: Record<string, unknown>) => {
@@ -66,19 +90,28 @@ const GenerateTransaction = () => {
           <FormLabel {...helperTextProps} fontWeight={600}>
             Key Type
           </FormLabel>
-          <Select
-            options={keyTypes.map(({ name, value, iconImage }) => ({
-              label: name,
-              value,
-              iconImage,
-            }))}
-            placeholder="Select..."
-            {...register('keyType')}
-            components={{
-              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-              // @ts-expect-error
-              Option: KeyTypeOption,
-            }}
+          <Controller
+            name="keyType"
+            control={control}
+            render={({ field }) => (
+              <Select
+                {...field}
+                options={keyTypes.map(({ name, value, iconImage }) => ({
+                  label: name,
+                  value,
+                  iconImage,
+                }))}
+                onChange={e => {
+                  field.onChange(e);
+                }}
+                placeholder="Select..."
+                components={{
+                  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                  // @ts-expect-error
+                  Option: KeyTypeOption,
+                }}
+              />
+            )}
           />
           <FormHelperText {...helperTextProps} color="--Sand-Light-11">
             No confirmation required
@@ -92,23 +125,33 @@ const GenerateTransaction = () => {
             <FormLabel {...helperTextProps} fontWeight={600}>
               Asset
             </FormLabel>
-            <Select
-              options={assets.map(
-                ({ name, value, iconImage, networkImage }) => ({
-                  label: name,
-                  value,
-                  iconImage,
-                  networkImage,
-                })
+            <Controller
+              name="asset"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  options={assets.map(
+                    ({ name, value, iconImage, networkImage }) => ({
+                      label: name,
+                      value,
+                      iconImage,
+                      networkImage,
+                    })
+                  )}
+                  onChange={e => {
+                    field.onChange(e);
+                  }}
+                  placeholder="Select..."
+                  components={{
+                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                    // @ts-expect-error
+                    Option: AssetOption,
+                  }}
+                />
               )}
-              placeholder="Select..."
-              {...register('asset')}
-              components={{
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-expect-error
-                Option: AssetOption,
-              }}
             />
+
             <FormHelperText {...helperTextProps} color="--Sand-Light-11">
               Ethereum Testnet Network{' '}
             </FormHelperText>
@@ -136,6 +179,7 @@ const GenerateTransaction = () => {
           </FormLabel>
           <NumberInput defaultValue={0.01} step={0.01}>
             <NumberInputField
+              {...register('amount')}
               h="40px"
               p="8px 12px"
               borderRadius="6px"
@@ -163,10 +207,29 @@ const GenerateTransaction = () => {
             42.331 available{' '}
           </FormHelperText>
           <FormErrorMessage>
-            {errors?.keyType?.message && errors.keyType.message.toString()}
+            {errors?.amount?.message && errors.amount.message.toString()}
+          </FormErrorMessage>
+        </FormControl>{' '}
+        <FormControl>
+          <FormLabel {...helperTextProps} fontWeight={600}>
+            Send To
+          </FormLabel>
+          <InputGroup>
+            <Input
+              {...register('address')}
+              placeholder="ETH address"
+              onFocus={handleAddressInputFocus}
+              onBlur={() => setIsAddressInputFocused(false)}
+            />
+            <InputRightElement>
+              <GenerateAddress isActive={isAddressInputFocused} />{' '}
+            </InputRightElement>
+          </InputGroup>
+          <FormErrorMessage>
+            {errors?.address?.message && errors.address.message.toString()}
           </FormErrorMessage>
         </FormControl>
-        <Button w="full" variant="black">
+        <Button w="full" variant="black" type="submit">
           Continue
         </Button>
       </Card>
